f23a9cb03904a8affe46473c0728125179a8b6d0
mahade_hasan <mahadehasan31@gmail.com>
2023-05-30 19:58:38 +0600

    validation messages added

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -21,7 +21,7 @@ export class RegisterComponent implements OnInit{
 
   initializeForm() {
     this.registerForm = new FormGroup({
-      username: new FormControl('Hello', Validators.required),
+      username: new FormControl('', Validators.required),
       password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
       confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
     });
@@ -32,7 +32,7 @@ export class RegisterComponent implements OnInit{
 
   matchValues(matchTo: string): ValidatorFn {
     return (control: AbstractControl) => {
-      return control.value === control.parent?.get(matchTo)?.value ? null : {noMatching: true}
+      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
     }
   }
 
